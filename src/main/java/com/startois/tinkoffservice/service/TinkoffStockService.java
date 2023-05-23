747c6347f0f7b8497857d6997ebb070cb0d23743
SeniorTomatos <dehuckoo5@gmail.com>
2023-05-23 23:48:27 +0300

    Есть проблема при запуске... надо решить

--- src/main/java/com/startois/tinkoffservice/service/TinkoffStockService.java
+++ src/main/java/com/startois/tinkoffservice/service/TinkoffStockService.java
@@ -1,19 +1,35 @@
 package com.startois.tinkoffservice.service;
 
+import com.startois.tinkoffservice.exception.StockNotFoundException;
+import com.startois.tinkoffservice.model.Currency;
 import com.startois.tinkoffservice.model.Stock;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.tinkoff.invest.openapi.OpenApi;
 
-public class TinkoffStockService implements StockService{
+@Service
+@RequiredArgsConstructor
+public class TinkoffStockService implements StockService {
     private final OpenApi openApi;
+
     @Override
     public Stock getStockByTicker(String ticker) {
         var context = openApi.getMarketContext();
         var listCF = context.searchMarketInstrumentsByTicker(ticker);
         var list = listCF.join().getInstruments();
         if (list.isEmpty()) {
-            
+            throw new StockNotFoundException(String.format("Stock %s not found", ticker));
         }
-        return null;
+
+        var item = list.get(0);
+        return new Stock(
+                item.getTicker(),
+                item.getFigi(),
+                item.getName(),
+                item.getType().getValue(),
+                Currency.valueOf(item.getCurrency().getValue()),
+                "TINKOFF"
+        );
     }
 
 
